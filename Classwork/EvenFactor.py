03a1a1fb409d4dcde0049c6cb445e3ac86d6366c
rishurwt <rishabhsinghrawat2021@gmail.com>
2026-03-23 10:05:35 +0530

    Delete EvenFactor.py

--- Classwork/EvenFactor.py
+++
@@ -1,9 +0,0 @@
-num = int(input("Enter a number: "))
-for i in range(2, num + 1):
-    
-    if num % i == 0 and i % 2 == 0:
-        print("Even factors of", num, "are:")
-        print(i)
-
-    else:
-        print("No even factors found!")
