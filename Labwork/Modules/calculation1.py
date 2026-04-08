2e0ac6344c0fb0b3695d06241a50631c4fb6aa4b
rishurwt <rishabhsinghrawat2021@gmail.com>
2026-04-08 10:49:35 +0530

    module in python

--- Labwork/Modules/calculation1.py
+++ Labwork/Modules/calculation1.py
@@ -2,4 +2,4 @@ import numericCalculation
 m = 10 
 n = 14
 print("Addition of ",m,"and ",n,"is ",numericCalculation.add(m,n))
-print("Product of ",m,"and ",n,"is ",numericCalculation.CalculateProduct(m,n))
+print("Product of ",m,"and ",n,"is ",numericCalculation.calculateProduct(m,n))
--- Labwork/Modules/calculation2.py
+++ Labwork/Modules/calculation2.py
@@ -2,4 +2,4 @@ from numericCalculation import *
 m = 10 
 n = 14
 print("Addition of ",m,"and ",n,"is ",add(m,n))
-print("Product of ",m,"and ",n,"is ",CalculateProduct(m,n))
+print("Product of ",m,"and ",n,"is ",calculateProduct(m,n))
