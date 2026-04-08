dcb86c55b06ba117366ec002770d10fee4f1904e
rishurwt <rishabhsinghrawat2021@gmail.com>
2026-04-08 10:50:11 +0530

    optimised module use

--- Labwork/Modules/calculation3.py
+++ Labwork/Modules/calculation3.py
@@ -4,4 +4,4 @@ from numericCalculation import addNumbers
 m = 10 
 n = 14
 print("Sum of ",m,"and ",n,"is ",addNumbers(m,n))
-print("product of ",m,"and ",n,"is ",calculateProduct(m,n))#exception throw hoga aaega
+# print("product of ",m,"and ",n,"is ",calculateProduct(m,n))#exception throw hoga aaega
