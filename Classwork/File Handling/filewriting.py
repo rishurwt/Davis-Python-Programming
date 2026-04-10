2b070373b405a4b77cb05882eaa7f1b8927fc8fe
rishurwt <rishabhsinghrawat2021@gmail.com>
2026-04-10 10:07:00 +0530

    File Handling : Write method

---
+++ Classwork/File Handling/filewriting.py
@@ -0,0 +1,9 @@
+filev = open("first.txt","w")
+print('Enter Sentence : ')
+for i in range(5):
+    sentence = input()
+    filev.write(sentence)
+    print('------------')
+print("Data Successfully written")
+filev.close()
+print(filev.read)
